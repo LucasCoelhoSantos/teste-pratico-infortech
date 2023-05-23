ef57623ad7954c0010a1b36d8c74ca76de16f763
Lucas Coelho Santos <lucascoelho375@gmail.com>
2023-05-23 19:58:32 -0400

    add: factory connection factory

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -1,5 +1,16 @@
 package factory;
 
-public class ConnectionFactory {
+import util.DatabaseConfig;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    // Conexão com o banco de dados
+    public static Connection createConnectionToMySQL() throws SQLException, ClassNotFoundException {
+    	Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(DatabaseConfig.getDatabaseUrl(), DatabaseConfig.getUsername(), DatabaseConfig.getPassword());
+        return connection;
+    }
 }
